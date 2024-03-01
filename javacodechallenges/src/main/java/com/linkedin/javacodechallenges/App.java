fac7a7ea13287500585b28ecf1ff5a882c282089
Ron Jordan <36054659+rsjordan@users.noreply.github.com>
2024-03-01 02:12:46 +0000

    Implement Person

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -4,6 +4,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        
+        Person p = new Person("Ron", "Jordan", 59);
+        System.out.println(p.introduction());
     }
 }
--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/Person.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/Person.java
@@ -1,3 +1,36 @@
+package com.linkedin.javacodechallenges;
+
 public class Person {
+  private String firstName;
+  private String lastName;
+  private int age;
+
+  public Person (String firstName, String lastName, int age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
   
+  public String getFirstName() {
+    return firstName;
+  }
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+  public String getLastName() {
+    return lastName;
+  }
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+  public int getAge() {
+    return age;
+  }
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public String introduction() {
+    return String.format("Hi, my name is %s %s and I'm %d years old", firstName, lastName, age);
+  }
 }
