a49ad5375c5083bde56a217d8e1423f7cdd22e4b
Ron Jordan <36054659+rsjordan@users.noreply.github.com>
2024-03-01 04:48:41 +0000

    Implement calculateHundredDaysFromNow

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,13 +1,21 @@
 package com.linkedin.javacodechallenges;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+
 public class App 
 {
     // Create function to calculate the date that's 
     // 100 days from now
+    public static LocalDate calculateHundredDaysFromNow(LocalDate today) {
+        return today.plusDays(100);
+    }
 
     public static void main( String[] args )
     {
+        System.out.println("Now is... "
+            + LocalDate.now(ZoneId.of(ZoneId.SHORT_IDS.get("PST"))));
         System.out.println("100 days from now is... " 
-            /* add function call */);
+            + calculateHundredDaysFromNow(LocalDate.now(ZoneId.of(ZoneId.SHORT_IDS.get("PST")))));
     }
 }
