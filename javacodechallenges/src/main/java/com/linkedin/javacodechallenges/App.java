0934d19931a0e0e72d25d61c19757d30bd6ac977
Ron Jordan <36054659+rsjordan@users.noreply.github.com>
2024-03-01 07:30:22 +0000

    Implement findStudentsWithIncompleteVolunteerEvents

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,5 +1,7 @@
 package com.linkedin.javacodechallenges;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -7,8 +9,35 @@ public class App {
     public static List<String> findStudentsWithIncompleteVolunteerEvents(
             List<String> students,
             Map<String, List<String>> attendeesMapping) {
-        // TODO: implement function
-        return List.of();
+
+        int COMPLETE_MINIMUM = 2;
+        // System.out.println("RSJ DEBUG - INPUT students: " + students);
+        // System.out.println("RSJ DEBUG - INPUT attendeesMapping: " + attendeesMapping);
+
+        List<String> studentsWithIncompleteVolunteerEvents = new ArrayList<>();
+
+        Map<String, Integer> nameToCountMap = new HashMap<>();
+        for (List<String> value : attendeesMapping.values()) {
+            value.stream().forEach(name -> {
+                int newCount;
+                if (!nameToCountMap.containsKey(name)) {
+                    newCount = 1;
+                } else {
+                    newCount = nameToCountMap.get(name) + 1;
+                }
+                nameToCountMap.put(name, newCount);
+            });
+        }
+        // System.out.println("RSJ DEBUG - nameToCountMap: " + nameToCountMap);
+
+        for (String student : students) {
+            if (!nameToCountMap.containsKey(student) || (nameToCountMap.get(student) < COMPLETE_MINIMUM)) {
+                studentsWithIncompleteVolunteerEvents.add(student);
+            }
+        }
+        
+        // System.out.println("RSJ DEBUG - studentsWithIncompleteVolunteerEvents: " + studentsWithIncompleteVolunteerEvents);
+        return studentsWithIncompleteVolunteerEvents;
     }
 
     public static void main(String[] args) {
@@ -22,6 +51,14 @@ public class App {
 
         System.out.println(findStudentsWithIncompleteVolunteerEvents(
                 students, attendeesMapping));
+
+        List<String> studentSet1 = List.of("Sally", "Polly", "Molly", "Tony", "Harry");
+        Map<String, List<String>> attendeesMapping1 = Map.of("Farmer's Market", List.of("Sally", "Polly"),
+                "Car Wash Fundraiser", List.of("Molly", "Tony", "Polly"),
+                "Cooking Workshop", List.of("Sally", "Molly", "Polly"),
+                "Midnight Breakfast", List.of("Polly", "Molly"));
+        System.out.println(findStudentsWithIncompleteVolunteerEvents(
+                studentSet1, attendeesMapping1));
     }
 
 }
