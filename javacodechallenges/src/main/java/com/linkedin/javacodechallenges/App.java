0614a64814fa3d5de537a9244a46deceb3d5ab0f
Ron Jordan <36054659+rsjordan@users.noreply.github.com>
2024-03-01 02:00:29 +0000

    Implement calculateWaterBill

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -3,16 +3,24 @@ package com.linkedin.javacodechallenges;
 import java.util.Scanner;
 
 public class App {
+    private static final double CCF = 748;
+    private static final double MINIMUM_CHARGE = 18.84;
+    private static final double COST_PER_ADDITIONAL_CCF = 3.90;
 
     public static double calculateWaterBill(double gallonsUsage) {
-        // TODO: Implement method
-        return 0;
+        double cost = MINIMUM_CHARGE;
+        if (gallonsUsage > (2 * CCF)) {
+            double overBaseline = gallonsUsage - (2 * CCF);
+            double extraCCFs = Math.ceil(overBaseline / CCF);
+            cost = cost + (extraCCFs * COST_PER_ADDITIONAL_CCF);
+        }
+        return cost;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("How many gallons of water did you " +
-                "use this month?");
+        System.out.print("How many gallons of water did you " +
+                "use this month? ");
         double usage = scanner.nextDouble();
         System.out.println("Your water bill is " +
                 calculateWaterBill(usage));
